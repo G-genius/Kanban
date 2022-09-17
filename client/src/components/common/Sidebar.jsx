bee2d408a075b2db92f8306c5f4b6abd7191ed18
G-genius <gamelob25@outlook.com>
2022-09-17 23:28:24 +0300

    Создание sidebar

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import {useSelector} from "react-redux";
+import "../../css/Sidebar.css"
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
+
+    // Навигация
+    const navigate = useNavigate()
+
+    // Получаем user name пользователя
+    const user = useSelector((state) => state.user.value)
+
+    const exitBtn = () => {
+        localStorage.removeItem("token")
+        navigate("/login")
+    }
+
     return (
-        <div>
-            Sidebar
+        <div className="side-bar">
+            <div className="user">
+                <a className="user-name">Пользователь:
+                    <a> {user.username} </a>
+                </a>
+                <a className="exit-btn" onClick={exitBtn}>Выйти</a>
+            </div>
         </div>
     )
 }
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../css/Home.css"
 
 const Home = () => {
     return (
-        <div>
-            Home
+        <div className="home">
+            <button className="create-board-btn">Создать первую доску</button>
         </div>
     )
 }
