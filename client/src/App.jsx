c57fdb734725418366f0bbc0ff01a46102517bf3
G-genius <gamelob25@outlook.com>
2022-10-31 20:21:35 +0300

    удаление шрифта

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,3 @@
-import '@fontsource/roboto/300.css'
-import '@fontsource/roboto/400.css'
-import '@fontsource/roboto/500.css'
-import '@fontsource/roboto/700.css'
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import AuthLayout from "./components/layout/AuthLayout";
 import Login from "./pages/Login";
